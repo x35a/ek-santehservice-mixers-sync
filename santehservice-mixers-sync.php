ab332a12374f463a11343688922d7ee958e566b8
xj <xj@email.com>
2025-08-08 15:46:01 +0300

    add santehservice-mixers-sync

---
+++ santehservice-mixers-sync.php
@@ -0,0 +1,4 @@
+<?php
+// file_put_contents(__DIR__ . '/cron_test_log.txt', "Script executed at " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+echo "Cron job executed successfully at " . date('Y-m-d H:i:s') . "\n";
+?>
